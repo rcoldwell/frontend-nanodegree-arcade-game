e00ad805b05659526fad773631f4524592b89acd
rcoldwell <robert@icoldwell.com>
2015-02-03 02:06:20 -0600

    Added gem, comments, readme

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 frontend-nanodegree-arcade-game
 ===============================
 
+R Coldwell:
+This game supports up/down/left/right control of the main character.
+Hitting a bug moves you back to the starting row.
+
+
+
+Udacity note:
+
 Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
 
 for self-checking their submission.
--- js/engine.js
+++ js/engine.js
@@ -95,6 +95,7 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+        gem.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -153,6 +154,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        gem.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -172,7 +174,8 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/GemGreen.png'
     ]);
     Resources.onReady(init);
 
